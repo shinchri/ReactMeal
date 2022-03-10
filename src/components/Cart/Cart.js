bd31022ea76c453fbced3f9d6bc1d53ed88a1582
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-03-10 10:14:40 -0600

    feat: Add modal for Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,11 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
-    const cartItems = [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>);
+    const cartItems = [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li key={item.id}>{item.name}</li>);
 
     return (
-        <div>
+        <Modal>
             <ul className={classes['cart-items']}>
                 {cartItems}
             </ul>
@@ -14,9 +15,9 @@ const Cart = props => {
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} >Close</button>
-                <button className={classes.buton}>Order</button>
+                <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 };
 
